1288fc3368cda47d0763d5f3c99b0410ba9b3154
Ariana Vidal <barbosavidala@gmail.com>
2021-10-15 21:03:34 -0300

    fim dos exercicios

--- Modulo-01-fundamentos-do-desenvolvimento-web/Bloco-04-JavaScript-e-Logica-de-Programacao/exercicio-4.1.js
+++ Modulo-01-fundamentos-do-desenvolvimento-web/Bloco-04-JavaScript-e-Logica-de-Programacao/exercicio-4.1.js
@@ -59,5 +59,85 @@ let movimentos = 'pra frente, pra trás, direita e esquerda';
 if (nomePeca === "torre"){
     console.log(movimentos)
 }else{
-    console.log(peça inexistente)
+    console.log('peça inexistente')
 }
+
+//* exercicio 7
+const grade = 50;
+
+if (grade < 0 || grade > 100) {
+  console.log("Erro, nota incorreta!");
+} else if (grade >= 90) {
+  console.log("A");
+} else if (grade >= 80) {
+  console.log("B");
+} else if (grade >= 70) {
+  console.log("C");
+} else if (grade >= 60) {
+  console.log("D");
+} else if (grade >= 50) {
+  console.log("E");
+} else {
+  console.log("F");
+}
+
+//* exercicio 8
+const n1 = 10;
+const n2 = 17;
+const n3 = 30;
+
+if (n1 && n2 || n3 / 2){
+    console.log('true')
+}else{
+    console.log('false')
+}
+
+//* exercicio 9
+const n1 = 10;
+const n2 = 17;
+const n3 = 30;
+
+if (n1 % 2 !== 0 || n2 % 2 !== 0 || n3 % 2 !== 0){
+    console.log('true')
+}else{
+    console.log('false')
+}
+
+//* exercicio 10
+const valorCusto = 12;
+const valorVenda = 22;
+const quantidade = 1000;
+const lucro = valorVenda - valorCusto;
+
+console.log(lucro * quantidade)
+
+//* exercicio 11
+const salarioBruto = 3500.00;
+let inss;
+let impostoRenda;
+
+if (salarioBruto <= 1556.94) {
+    inss = salarioBruto * 0.008;
+}else if(salarioBruto <= 2594.92) {
+    inss = salarioBruto * 0.009;
+}else if (salarioBruto <= 5189.82) {
+    inss= salarioBruto * 0.11;
+}else{
+    inss = 570.88;
+}
+
+const salarioBase = salarioBruto - inss;
+
+if (salarioBase <= 1903.98){
+    impostoRenda = 0;
+}else if (salarioBase <= 2826.65){
+    impostoRenda = (salarioBase * 0.075) - 142.80;
+}else if (salarioBase <= 3751.05){
+    impostoRenda = (salarioBase * 0.15) - 354.80;
+}else if (salarioBase <= 4664.68){
+    impostoRenda = (salarioBase * 0,225) - 636,13;
+}else if (salarioBase > 4664.68){
+    impostoRenda = (salarioBase * 0.275) - 869.36;
+}
+
+console.log("Salário:" + (salarioBase - impostoRenda));
