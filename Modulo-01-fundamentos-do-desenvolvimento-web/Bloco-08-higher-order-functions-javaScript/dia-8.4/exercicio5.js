049735ba443f19938b274f0422426473e1a03be0
Ariana Vidal <barbosavidala@gmail.com>
2021-11-24 08:48:00 -0300

    dia 8.4

---
+++ Modulo-01-fundamentos-do-desenvolvimento-web/Bloco-08-higher-order-functions-javaScript/dia-8.4/exercicio5.js
@@ -0,0 +1,15 @@
+const names = [
+    'Aanemarie', 'Adervandes', 'Akifusa',
+    'Abegildo', 'Adicellia', 'Aladonata',
+    'Abeladerco', 'Adieidy', 'Alarucha',
+  ];
+  
+  function containsA() {
+    return names.reduce((acc, curr) => acc + curr.split('').reduce((acumulador, current) => {
+        if (current === 'a' || current === 'A'){
+            return acumulador + 1;
+        }
+        return acumulador;
+    },0),0);
+  }
+  console.log(containsA());
